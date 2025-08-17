16fceed2898e174aa023a37cb157a92b1f567943
GiovanniWitulski <witulski.schule@gmail.com>
2025-08-17 16:57:30 +0200

    add: add permissions (isOwnerOrMember, IsTaskOnAccessibleBoard & IsOwner)

---
+++ kanmind_app/api/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+
+class IsOwnerOrMember(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user or request.user in obj.members.all()
+
+class IsTaskOnAccessibleBoard(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        board = obj.board
+        return board.owner == request.user or request.user in board.members.all()
+    
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
