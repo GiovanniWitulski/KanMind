023060e1cd393ae56c6e13165a96d37698e43095
GiovanniWitulski <witulski.schule@gmail.com>
2025-08-17 16:45:21 +0200

    feat: add email-check url

--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -1,8 +1,11 @@
 
 from django.urls import path
-from .views import RegistrationView, LoginView
+from .views import RegistrationView, LoginView, UserProfileList, UserProfileDetail, EmailCheckView
 
 urlpatterns = [
+    path('profiles/', UserProfileList.as_view(), name='userprofile-list'),
+    path('profiles/<int:pk>/', UserProfileDetail.as_view(), name='userprofile-detail'),
     path('registration/', RegistrationView.as_view(), name='registration'),
     path('login/', LoginView.as_view(), name='login'),
+    path('email-check/', EmailCheckView.as_view(), name='email-check'),
 ]
