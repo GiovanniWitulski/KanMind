244da21a1282efce48ca4565b09517e6bb284260
GiovanniWitulski <witulski.schule@gmail.com>
2025-08-22 18:50:01 +0200

    add (permission class): added IsAuthorOrReadOnly

--- kanmind_app/api/permissions.py
+++ kanmind_app/api/permissions.py
@@ -12,3 +12,10 @@ class IsTaskOnAccessibleBoard(permissions.BasePermission):
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
+    
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
