aea1c1e831ca4fdcef071f4ed725c6eeeda7ab27
GiovanniWitulski <witulski.schule@gmail.com>
2025-08-22 18:45:43 +0200

    add: added Comment model

--- kanmind_app/models.py
+++ kanmind_app/models.py
@@ -64,3 +64,21 @@ class Board(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Comment(models.Model):
+    task = models.ForeignKey(
+        Task, 
+        on_delete=models.CASCADE, 
+        related_name='comments'
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.task.title}'
