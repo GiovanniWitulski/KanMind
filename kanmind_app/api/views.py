6817ba95776b79914cdf4ed7cb2b80a746d39a38
GiovanniWitulski <witulski.schule@gmail.com>
2025-08-22 18:56:02 +0200

    add(view): added comment view (ModelViewSet)

--- kanmind_app/api/views.py
+++ kanmind_app/api/views.py
@@ -1,10 +1,10 @@
 from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django.db.models import Q
-from ..models import Board, Task
-from .serializers import TaskSerializer, BoardSerializer, BoardUpdateSerializer, BoardDetailSerializer, TaskDetailSerializer
-from .permissions import IsOwnerOrMember, IsOwner, IsTaskOnAccessibleBoard
+from django.db.models import Q, Count 
+from ..models import Board, Task, Comment 
+from .serializers import TaskSerializer, BoardSerializer, BoardUpdateSerializer, BoardDetailSerializer, TaskDetailSerializer, CommentSerializer, CommentCreateSerializer 
+from .permissions import IsOwnerOrMember, IsOwner, IsTaskOnAccessibleBoard, IsAuthorOrReadOnly 
 
 class BoardViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
@@ -51,7 +51,9 @@ class TaskViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         user = self.request.user
         accessible_boards = Board.objects.filter(Q(owner=user) | Q(members=user))
-        return Task.objects.filter(board__in=accessible_boards)
+        return Task.objects.filter(board__in=accessible_boards).annotate(
+            comments_count=Count('comments')
+        )
     
     @action(detail=False, methods=['get'])
     def assigned_to_me(self, request):
@@ -67,3 +69,19 @@ class TaskViewSet(viewsets.ModelViewSet):
         tasks = Task.objects.filter(reviewer=user)
         serializer = TaskDetailSerializer(tasks, many=True, context={'request': request})
         return Response(serializer.data)
+    
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        task_pk = self.kwargs['task_pk']
+        return Comment.objects.filter(task_id=task_pk)
+
+    def perform_create(self, serializer):
+        task_pk = self.kwargs['task_pk']
+        task = Task.objects.get(pk=task_pk)
+        serializer.save(author=self.request.user, task=task)
