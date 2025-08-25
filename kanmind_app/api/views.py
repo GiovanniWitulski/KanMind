2d039890e6848cb16f2c71b4bb325b8813d2eeb3
GiovanniWitulski <witulski.schule@gmail.com>
2025-08-25 21:30:10 +0200

    refacture(TaskViewSet): changed the way the task view selects serializers + fixed an issue whrer the member count was one off

--- kanmind_app/api/views.py
+++ kanmind_app/api/views.py
@@ -1,18 +1,20 @@
 from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 from django.db.models import Q, Count 
 from ..models import Board, Task, Comment 
 from .serializers import TaskSerializer, BoardSerializer, BoardUpdateSerializer, BoardDetailSerializer, TaskDetailSerializer, CommentSerializer, CommentCreateSerializer 
-from .permissions import IsOwnerOrMember, IsOwner, IsTaskOnAccessibleBoard, IsAuthorOrReadOnly 
+from .permissions import IsOwnerOrMember, IsOwner, IsTaskOnAccessibleBoard, IsAuthorOrReadOnly, CanDeleteTask 
 
 class BoardViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.action == 'destroy':
             permission_classes = [permissions.IsAuthenticated, IsOwner]
+        elif self.action in ['update', 'partial_update']:
+             permission_classes = [permissions.IsAuthenticated, IsOwnerOrMember]
         else:
-            permission_classes = [permissions.IsAuthenticated, IsOwnerOrMember]
-            
+            permission_classes = [permissions.IsAuthenticated]
         return [permission() for permission in permission_classes]
 
     def get_queryset(self):
@@ -24,15 +26,29 @@ class BoardViewSet(viewsets.ModelViewSet):
             return BoardDetailSerializer
         
         if self.action in ['create', 'update', 'partial_update']:
-            return BoardUpdateSerializer
-            
+            return BoardUpdateSerializer        
         return BoardSerializer
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-    
+        board_instance = serializer.save(owner=self.request.user)
+        board_instance.members.add(self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        response_serializer = BoardSerializer(serializer.instance, context=self.get_serializer_context())
+        
+        headers = self.get_success_headers(response_serializer.data)
+        return Response(response_serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
     def partial_update(self, request, *args, **kwargs):
         instance = self.get_object()
+        
+        if not (instance.owner == request.user or request.user in instance.members.all()):
+             raise PermissionDenied("You must be the owner or a member to edit this board.")
+
         serializer = self.get_serializer(instance, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
@@ -46,7 +62,14 @@ class BoardViewSet(viewsets.ModelViewSet):
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
-    permission_classes = [permissions.IsAuthenticated, IsTaskOnAccessibleBoard]
+
+    def get_permissions(self):
+
+        if self.action == 'destroy':
+            permission_classes = [permissions.IsAuthenticated, CanDeleteTask]
+        else:
+            permission_classes = [permissions.IsAuthenticated, IsTaskOnAccessibleBoard]
+        return [permission() for permission in permission_classes]
 
     def get_queryset(self):
         user = self.request.user
@@ -55,19 +78,28 @@ class TaskViewSet(viewsets.ModelViewSet):
             comments_count=Count('comments')
         )
     
+    def perform_create(self, serializer):
+        board = serializer.validated_data['board']
+        is_owner = board.owner == self.request.user
+        is_member = self.request.user in board.members.all()
+
+        if not (is_owner or is_member):
+            raise PermissionDenied("You do not have permission to create a task on this board.")
+        
+        serializer.save(created_by=self.request.user)
+
     @action(detail=False, methods=['get'])
     def assigned_to_me(self, request):
         user = self.request.user
-        tasks = Task.objects.filter(assignee=user)
-        serializer = TaskDetailSerializer(tasks, many=True, context={'request': request})
+        tasks = self.get_queryset().filter(assignee=user)
+        serializer = self.get_serializer(tasks, many=True)
         return Response(serializer.data)
     
-        
     @action(detail=False, methods=['get'], url_path='reviewing')
     def reviews_for_me(self, request):
         user = self.request.user
-        tasks = Task.objects.filter(reviewer=user)
-        serializer = TaskDetailSerializer(tasks, many=True, context={'request': request})
+        tasks = self.get_queryset().filter(reviewer=user) 
+        serializer = self.get_serializer(tasks, many=True)
         return Response(serializer.data)
     
 
