a5defe87ba0952087f53cce5c81db854bde40513
GiovanniWitulski <witulski.schule@gmail.com>
2025-09-01 17:46:55 +0200

    feat: Add Models to admin panel

--- user_auth_app/admin.py
+++ user_auth_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import User, Group
+from .models import UserProfile
 # Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    admin.site.register(UserProfile)
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
