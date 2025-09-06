6f1b2981d41e85107b2f5df04f2a8105404647f4
GiovanniWitulski <witulski.schule@gmail.com>
2025-09-06 03:55:24 +0200

    refactor(urls): Removed unused router

--- kanmind_app/api/urls.py
+++ kanmind_app/api/urls.py
@@ -2,13 +2,10 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter, SimpleRouter
 from . import views
 
-router = DefaultRouter()
-
 comment_router = SimpleRouter()
 comment_router.register(r'', views.CommentViewSet, basename='task-comments')
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('boards/', views.BoardListCreateView.as_view(), name='board-list-create'),
     path('boards/<int:pk>/', views.BoardDetailView.as_view(), name='board-detail'),
     path('tasks/', views.TaskListCreateView.as_view(), name='task-list-create'),
